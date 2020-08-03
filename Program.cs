ba3958c999dbf9f0d88c0335ca0e2e7bced684c7
Ann Hagan <ann.marie783@gmail.com>
2020-08-03 10:40:36 -0500

    Adding if/Else statement to method

--- Program.cs
+++ Program.cs
@@ -8,19 +8,31 @@ namespace ooP
         {
             //create an object of my class
             //instance of the class
-           human denis = new human("denis", "Wagner", "brown");
+           human denis = new human("denis", "Wagner", "brown", 30);
            //access public variable from outside, and even change it.
            //alternative method --denis.firstName = "Denis";
            // alternative method denis.lastName = "Wagner";
            //call methods of the class
            denis.IntroducingMyself();
 
-           human ann = new human("Ann", "Hagan", "Blue");
+           human ann = new human("Ann", "Hagan", "Blue", 42);
           // ann.firstName = "Ann";
            //ann.lastName = "Hagan";
 
-           ann.IntroducingMyself();
+            human billy = new human("Bill", "Hagan", "Blue", 49);
+            human naomi = new human("Naomi", "Abell", "Hazel", 0);
+            human bingo = new human("Bingo");
 
+
+           ann.IntroducingMyself();
+           billy.IntroducingMyself();
+           naomi.IntroducingMyself();
+           bingo.IntroducingMyself();
+        
+
+
+            human basicHuman = new human();
+            basicHuman.IntroducingMyself();
 
            Console.ReadKey();
         }
--- human.cs
+++ human.cs
@@ -9,19 +9,53 @@ namespace ooP
         public string lastName;
         private string eyeColor;
 
-        //constructor
-        public human(string firstName, string lastName, string eyeColor)
+        private int age;
+
+        //default constuctor
+        public human() {
+            Console.WriteLine("Constructor called. Object created");
+
+        }
+
+        public human(string firstName)
+        {
+            this.firstName = firstName;
+            
+        }
+       public human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        //paremeterized constructor
+        public human(string firstName, string lastName, string eyeColor, int age)
         {
             this.firstName = firstName;
             this.lastName = lastName;
             this.eyeColor = eyeColor;
+            this.age = age;
         }
 
         //member method
         public void IntroducingMyself()
         {
-            Console.WriteLine("hi, I'm {0} {1}", firstName, lastName);
-            Console.WriteLine("My eye color is {0}", eyeColor);
+            if(age != 0 && lastName != null && eyeColor != null && firstName != null) {
+                Console.WriteLine("hi, I'm {0} {1}.", firstName, lastName);
+                Console.WriteLine("My eye color is {0} and I am {1}  years old.", eyeColor, age);
+            }
+            else  if(age != 0 && lastName != null && firstName != null)
+            {
+                Console.WriteLine("hi, I'm {0} {1}", firstName, lastName);
+            }
+            else if (age != 0 && firstName != null)
+            {
+                Console.WriteLine("hi I am {0} and I am {1} years old.", firstName, age);
+            }
+            else if (firstName != null)
+            {
+                 Console.WriteLine("hi I am {0}.", firstName);
+            }
         }
     }
 }
